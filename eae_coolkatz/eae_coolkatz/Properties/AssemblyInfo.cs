18b8d59cc199d5a5589d7059d4460189427b8c2c
conankzhang <u0409453@utah.edu>
2017-09-09 19:58:45 -0600

    Icon and updated Assembly Info

--- eae_coolkatz/eae_coolkatz/Properties/AssemblyInfo.cs
+++ eae_coolkatz/eae_coolkatz/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MonsterMania3")]
+[assembly: AssemblyTitle("Monster Mania 3")]
 [assembly: AssemblyProduct("eae_coolkatz")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("Cool Katz Interactive")]
-[assembly: AssemblyCopyright("Copyright © College of Engineering 2017")]
+[assembly: AssemblyCopyright("Copyright © Cool Katz Interactive 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.4.0.456")]
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0")]
