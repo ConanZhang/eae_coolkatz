2250c7ea6b99fc620b88b054246c7705cf0c1092
conankzhang <u0409453@utah.edu>
2017-09-09 19:53:24 -0600

    Added missing mp3 and commented out debug

--- eae_coolkatz/eae_coolkatz/Screens/GameplayScreen.cs
+++ eae_coolkatz/eae_coolkatz/Screens/GameplayScreen.cs
@@ -1061,7 +1061,7 @@ namespace eae_coolkatz.Screens
             spriteBatch.End();
 
             spriteBatch.Begin();
-            debug.RenderDebugData(ref camera.SimProjection, ref camera.SimView);
+            //debug.RenderDebugData(ref camera.SimProjection, ref camera.SimView);
             base.Draw(spriteBatch);
         }
 
